71de3f0de3cff480a6576c75dd0ff7e00db29867
Lloyd Preston <lloydpreston@outlook.com>
2026-03-04 18:19:14 +0000

    Age verification criteria to include supervision

--- 01_data_types.py
+++ 01_data_types.py
@@ -1,11 +1,16 @@
 #  *if/else age verification*
 
-#   This code will determine wether a user is allowed to enter using "if/else" with the criteria being 18 or over.
+#   This code will determine wether a user is allowed to enter using "if/else" with the criteria being 18 or 16 with adult supervision.
 
 user_age = 20
 required_age = 18
+adult_supervision = 16
 
 if user_age >= required_age:
     print("Allowed to enter")
+
+elif user_age >= 16:
+    print("Adult supervision required")
+
 else:
     print("Denied access")
