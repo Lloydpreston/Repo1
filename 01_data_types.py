b21ccc5d82e9f4df0e6b64038562589d5a51ca94
Lloydpreston <lloydpreston@outlook.com>
2026-03-04 17:48:12 +0000

    Add age verification logic in 01_data_types.py
    Implement age verification using if/else statement.

---
+++ 01_data_types.py
@@ -0,0 +1,11 @@
+#  *if/else age verification*
+
+#   This code will determine wether a user is allowed to enter using "if/else" with the criteria being 18 or over.
+
+user_age = 20
+required_age = 18
+
+if user_age >= required_age:
+    print("Allowed to enter")
+else:
+    print("Denied access")
